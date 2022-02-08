e1a3829be1c3dade70b9fbaccb797cc92e4c1ad3
ivan-gavran <ivan@informal.systems>
2022-02-08 18:04:51 +0100

    json parsing to a pre-defined struct

--- x/authz/keeper/mbt_test.go
+++ x/authz/keeper/mbt_test.go
@@ -38,33 +38,64 @@ type ExecMessageModel struct {
 	Validator      string `json:"validator"`
 }
 
-func GiveSpecifiedGrant(actionTaken interface{}, ctx sdk.Context) {
+type ValidatorList struct {
+	Validators []string `json:"set"`
+}
+
+type GrantPayloadModel struct {
+	Limit        int           `json:"limit"`
+	SpecialValue string        `json:"special_value"`
+	StakingType  string        `json:"staking_type"`
+	AllowList    ValidatorList `json:"allow_list"`
+	DenyList     ValidatorList `json:"deny_list"`
+}
+
+type ActionModel struct {
+	ActionType        string            `json:"action_type"`
+	AuthorizationType string            `json:"authorization_type"`
+	ExecMessage       ExecMessageModel  `json:"exec_message"`
+	GrantPayload      GrantPayloadModel `json:"grant_payload"`
+	Granter           string            `json:"granter"`
+	Grantee           string            `json:"grantee"`
+}
+
+type StateModel struct {
+	Meta          interface{} `json:"#meta"`
+	ActionTaken   ActionModel `json:"action_taken"`
+	ActiveGrants  interface{} `json:"active_grants"`
+	ExpiredGrants interface{} `json:"expired_grants"`
+	NumExecs      int         `json:"num_execs"`
+	NumGrants     int         `json:"num_grants"`
+	OutcomeStatus string      `json:"outcome_status"`
+}
+
+type MainJsonStruct struct {
+	Meta   interface{}   `json:"#meta"`
+	Vars   []interface{} `json:"vars"`
+	States []StateModel  `json:"states"`
+}
+
+func GiveSpecifiedGrant(actionTaken ActionModel, ctx sdk.Context) {
 	// granter := sdk.ValAddress(actionTaken.(map[string]interface{})["granter"].(string))
 	// grantee := sdk.ValAddress(actionTaken.(map[string]interface{})["grantee"].(string))
-	authorizationType := actionTaken.(map[string]interface{})["authorization_type"].(string)
+	authorizationType := actionTaken.AuthorizationType
 	fmt.Println(authorizationType)
 
 	switch authorizationType {
 	case "stake":
 		{
-			grantPayload := actionTaken.(map[string]interface{})["grant_payload"]
-			allowListIntermediate := grantPayload.(map[string]interface{})["allow_list"]
-			allowList := allowListIntermediate.(map[string]interface{})["#set"]
-			allowListMap := make(map[string]bool)
-			for _, validator := range allowList.([]interface{}) {
-				// fmt.Println(validator.(string), reflect.TypeOf(validator.(string)))
-				allowListMap[validator.(string)] = true
-			}
-
-			fmt.Println(allowListMap, reflect.TypeOf(allowList))
+			grantPayload := actionTaken.GrantPayload
+			allowList := grantPayload.AllowList.Validators
+			denyList := grantPayload.DenyList.Validators
+			fmt.Println(allowList, reflect.TypeOf(allowList))
 		}
 	}
 }
 
-func ExecuteAppropriateAction(t *testing.T, state interface{}, ctx sdk.Context) {
-	actionTaken := state.(map[string]interface{})["action_taken"]
-	actionType := actionTaken.(map[string]interface{})["action_type"].(string)
-	switch ActionType(actionType) {
+func ExecuteAppropriateAction(state StateModel, ctx sdk.Context, t *testing.T) {
+	actionTaken := state.ActionTaken
+
+	switch ActionType(actionTaken.ActionType) {
 	case GiveGrant:
 		GiveSpecifiedGrant(actionTaken, ctx)
 
@@ -83,16 +114,10 @@ func TestExecuteItfJson(t *testing.T) {
 	defer traceFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(traceFile)
-	var dat map[string]interface{}
-
-	if err := json.Unmarshal(byteValue, &dat); err != nil {
-		panic(err)
-	}
-
-	states := dat["states"].([]interface{})
-	for _, state := range states {
-		ExecuteAppropriateAction(t, state, ctx)
-
+	var jsonData MainJsonStruct
+	json.Unmarshal(byteValue, &jsonData)
+	for _, state := range jsonData.States {
+		ExecuteAppropriateAction(state, ctx, t)
 	}
 
 	require.True(t, false)
