c6619d6d03d2402121d60408e4261974b09f4d0a
ivan-gavran <ivan@informal.systems>
2022-02-16 11:54:09 +0100

    first working version

--- x/authz/keeper/mbt_test.go
+++ x/authz/keeper/mbt_test.go
@@ -8,12 +8,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta2"
+	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -21,6 +23,10 @@ import (
 
 var app *simapp.SimApp
 var ctx sdk.Context
+var defaultRecipient sdk.AccAddress
+var accounts map[string]sdk.AccAddress
+var validators map[string]sdk.ValAddress
+var PKs []types.PubKey
 
 const GRANT_SUCCESS string = "grant_success"
 const GRANT_FAILED string = "grant_failed"
@@ -37,6 +43,16 @@ const (
 	ExecuteGrant ActionType = "execute grant"
 )
 
+type MessageType string
+
+const (
+	MsgDelegate   MessageType = "msg_delegate"
+	MsgUndelegate MessageType = "msg_undelegate"
+	MsgRedelegate MessageType = "msg_redelegate"
+	MsgSend       MessageType = "msg_send"
+	MsgOther      MessageType = "msg_alpha"
+)
+
 // exec_message": {
 // 	"amount": -1,
 // 	"message_type": "",
@@ -48,6 +64,7 @@ type ExecMessageModel struct {
 	Amount       int    `json:"amount"`
 	Message_Type string `json:"message_type"`
 	Validator    string `json:"validator"`
+	NewValidator string `json:"new_validator"`
 }
 
 type ValidatorList struct {
@@ -94,37 +111,24 @@ type MainJsonStruct struct {
 func getValAddrList(names []string) []sdk.ValAddress {
 	addrList := make([]sdk.ValAddress, len(names))
 	for i, name := range names {
-		addrList[i] = sdk.ValAddress(name)
+		addrList[i] = validators[name]
 	}
 
 	return addrList
 }
 
-func stakeMsgTlaToCosmosURL(msg_type string) (string, error) {
-	switch msg_type {
-	case "msg_delegate":
-		return sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}), nil
-	case "msg_undelegate":
-		return sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}), nil
-	case "msg_redelegate":
-		return sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}), nil
-	default:
-		return "", sdkerrors.ErrInvalidType.Wrapf("unknown authorization for message type %T", msg_type)
-	}
-}
-
 func genericMsgTlaToCosmosURL(msg_type string) (string, error) {
 
 	switch msg_type {
-	case "msg_delegate":
+	case string(MsgDelegate):
 		return sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}), nil
-	case "msg_undelegate":
+	case string(MsgUndelegate):
 		return sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}), nil
-	case "msg_redelegate":
+	case string(MsgRedelegate):
 		return sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}), nil
-	case "msg_send":
+	case string(MsgSend):
 		return sdk.MsgTypeURL(&banktypes.MsgSend{}), nil
-	case "msg_alpha":
+	case string(MsgOther):
 		return sdk.MsgTypeURL(&govtypes.MsgVote{}), nil
 	default:
 		return "", fmt.Errorf("invalid message")
@@ -144,11 +148,11 @@ func giveSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 
 			var stakingAuthorizationType stakingtypes.AuthorizationType
 			switch actionTaken.Grant.SdkMessageType {
-			case "msg_delegate":
+			case string(MsgDelegate):
 				stakingAuthorizationType = stakingtypes.AuthorizationType_AUTHORIZATION_TYPE_DELEGATE
-			case "msg_undelegate":
+			case string(MsgUndelegate):
 				stakingAuthorizationType = stakingtypes.AuthorizationType_AUTHORIZATION_TYPE_UNDELEGATE
-			case "msg_redelegate":
+			case string(MsgRedelegate):
 				stakingAuthorizationType = stakingtypes.AuthorizationType_AUTHORIZATION_TYPE_REDELEGATE
 			default:
 				stakingAuthorizationType = stakingtypes.AuthorizationType_AUTHORIZATION_TYPE_UNSPECIFIED
@@ -158,7 +162,7 @@ func giveSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 			if grantPayload.SpecialValue == "inf" {
 				limitAddr = nil
 			} else {
-				limit := sdk.NewInt64Coin("testCoin", int64(grantPayload.Limit))
+				limit := sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(grantPayload.Limit))
 				limitAddr = &limit
 			}
 			var err error
@@ -178,7 +182,7 @@ func giveSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 		{
 
 			var limit sdk.Coins
-			limit = sdk.NewCoins(sdk.NewCoin("testCoin", sdk.NewInt(int64(grantPayload.Limit))))
+			limit = sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(int64(grantPayload.Limit))))
 			authorization = banktypes.NewSendAuthorization(limit)
 
 		}
@@ -195,8 +199,8 @@ func giveSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 	}
 	require.NoError(t, authorization.ValidateBasic())
 
-	granterAddr := sdk.AccAddress(actionTaken.Grant.Granter)
-	granteeAddr := sdk.AccAddress(actionTaken.Grant.Grantee)
+	granterAddr := accounts[actionTaken.Grant.Granter]
+	granteeAddr := accounts[actionTaken.Grant.Grantee]
 	now := ctx.BlockHeader().Time
 	err := app.AuthzKeeper.SaveGrant(ctx, granteeAddr, granterAddr, authorization, now.Add(time.Hour))
 	require.NoError(t, err)
@@ -216,8 +220,8 @@ func giveSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 }
 
 func revokeSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
-	granterAddr := sdk.AccAddress(actionTaken.Grant.Granter)
-	granteeAddr := sdk.AccAddress(actionTaken.Grant.Grantee)
+	granterAddr := accounts[actionTaken.Grant.Granter]
+	granteeAddr := accounts[actionTaken.Grant.Grantee]
 	authorizationType, err := genericMsgTlaToCosmosURL(actionTaken.Grant.SdkMessageType)
 	require.NoError(t, err)
 	err = app.AuthzKeeper.DeleteGrant(ctx, granteeAddr, granterAddr, authorizationType)
@@ -236,7 +240,7 @@ func revokeSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T)
 
 func expireSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
 	granterAddr := sdk.AccAddress(actionTaken.Grant.Granter)
-	granteeAddr := sdk.AccAddress(actionTaken.Grant.Grantee)
+	granteeAddr := accounts[actionTaken.Grant.Grantee]
 	authorizationType, err := genericMsgTlaToCosmosURL(actionTaken.Grant.SdkMessageType)
 	require.NoError(t, err)
 	authorization, _ := app.AuthzKeeper.GetCleanAuthorization(ctx, granteeAddr, granterAddr, authorizationType)
@@ -244,6 +248,60 @@ func expireSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T)
 	// expiring a grant is done by updating the previous authorization by a new one that is in the past
 	err = app.AuthzKeeper.SaveGrant(ctx, granteeAddr, granterAddr, authorization, now.Add(-1*time.Hour))
 	require.NoError(t, err)
+
+}
+
+func executeSpecifiedGrant(actionTaken ActionModel, outcome string, t *testing.T) {
+	granteeAddr := accounts[actionTaken.Grant.Grantee]
+	granterAddr := accounts[actionTaken.Grant.Granter]
+	// var msgs authz.MsgExec
+	var messages []sdk.Msg
+
+	switch actionTaken.ExecMessage.Message_Type {
+
+	case string(MsgSend):
+		amount := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(int64(actionTaken.ExecMessage.Amount))))
+		messages = []sdk.Msg{
+			banktypes.NewMsgSend(granteeAddr, defaultRecipient, amount),
+		}
+
+	case string(MsgDelegate):
+		amount := sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(actionTaken.ExecMessage.Amount))
+		valAddress := validators[actionTaken.ExecMessage.Validator]
+		messages = []sdk.Msg{
+			stakingtypes.NewMsgDelegate(granterAddr, valAddress, amount),
+		}
+
+	case string(MsgUndelegate):
+		amount := sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(actionTaken.ExecMessage.Amount))
+		valAddress := validators[actionTaken.ExecMessage.Validator]
+		messages = []sdk.Msg{
+			stakingtypes.NewMsgUndelegate(granterAddr, valAddress, amount),
+		}
+
+	case string(MsgRedelegate):
+		amount := sdk.NewInt64Coin(sdk.DefaultBondDenom, int64(actionTaken.ExecMessage.Amount))
+		valAddress := validators[actionTaken.ExecMessage.Validator]
+		newValAddress := validators[actionTaken.ExecMessage.NewValidator]
+		messages = []sdk.Msg{
+			stakingtypes.NewMsgBeginRedelegate(granterAddr, valAddress, newValAddress, amount),
+		}
+
+	default:
+		sdkerrors.ErrInvalidType.Wrapf("modelator: Invalid message type")
+	}
+
+	exec_message := authz.NewMsgExec(granteeAddr, messages)
+	executeMsgs, err := exec_message.GetMessages()
+	require.NoError(t, err)
+	result, err := app.AuthzKeeper.DispatchActions(ctx, granteeAddr, executeMsgs)
+
+	if outcome == "successful_auth_exec" {
+		require.NoError(t, err)
+		require.NotNil(t, result)
+	} else {
+		require.Nil(t, result)
+	}
 
 }
 
@@ -258,6 +316,10 @@ func executeAppropriateAction(state StateModel, t *testing.T) {
 		revokeSpecifiedGrant(actionTaken, outcome, t)
 	case ExpireGrant:
 		expireSpecifiedGrant(actionTaken, outcome, t)
+	case ExecuteGrant:
+		executeSpecifiedGrant(actionTaken, outcome, t)
+	case "":
+		// nothing here, "" is init state
 	default:
 		fmt.Printf("hi, nothing here for action type %s\n", actionTaken.ActionType)
 	}
@@ -267,6 +329,31 @@ func executeAppropriateAction(state StateModel, t *testing.T) {
 func TestExecuteItfJson(t *testing.T) {
 	app = simapp.Setup(t, false)
 	ctx = app.BaseApp.NewContext(false, tmproto.Header{})
+
+	PKs = simapp.CreateTestPubKeys(5)
+	accounts = make(map[string]sdk.AccAddress)
+	accountsAddresses := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(1000))
+	accounts["A"] = accountsAddresses[0]
+	accounts["B"] = accountsAddresses[1]
+	accounts["C"] = accountsAddresses[2]
+	accounts["default_recipient"] = accountsAddresses[3]
+
+	validators = make(map[string]sdk.ValAddress)
+	validatorAddresses := simapp.ConvertAddrsToValAddrs(simapp.AddTestAddrs(app, ctx, 3, sdk.NewInt(1000)))
+	validators["X"] = validatorAddresses[0]
+	validators["Y"] = validatorAddresses[1]
+	validators["Z"] = validatorAddresses[2]
+	tstaking := teststaking.NewHelper(t, ctx, app.StakingKeeper)
+	for i, valAddr := range validatorAddresses {
+		tstaking.CreateValidator(valAddr, PKs[i], sdk.NewInt(10), true)
+	}
+
+	for _, accAddr := range accountsAddresses {
+		for _, valAddr := range validatorAddresses {
+			tstaking.Delegate(accAddr, valAddr, sdk.NewInt(50))
+		}
+	}
+
 	traceFileName := "trace_example.itf.json"
 	traceFile, err := os.Open(traceFileName)
 	if err != nil {
@@ -280,7 +367,8 @@ func TestExecuteItfJson(t *testing.T) {
 	for _, state := range jsonData.States {
 		executeAppropriateAction(state, t)
 	}
-
+	// TODO: the question is: how to add validators to the context? I also want to have all the nodes
+	// have enough resources to send around
 	// TODO: in the last step, check the full state of active grants and their authorizations
 
 	require.True(t, false)
